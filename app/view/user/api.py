4f5a37795a22e24fd5aed24ce6ae4b4b58b9bd08
osangu <osangu.dev@gmail.com>
2024-06-28 14:02:42 +0900

    swagger details

--- app/view/user/api.py
+++ app/view/user/api.py
@@ -1,15 +1,19 @@
 from fastapi import APIRouter, Depends
 
 from .schema.request import UserRegisterRequest
+from ..swagger import SwaggerDetails
 
 from app.core.service.user import UserService
-from ...security.auth import Authorization
+from app.security.auth import Authorization
 
 user_router = APIRouter(prefix="/users")
 
 
 @user_router.post(
-    path='/register'
+    **SwaggerDetails.register,
+    response_model=None,
+    status_code=202,
+    path='/register',
 )
 def user_sign_up(
         request: UserRegisterRequest,
@@ -23,10 +27,13 @@ def user_sign_up(
 
 
 @user_router.post(
+    **SwaggerDetails.login,
+    response_model=None,
+    status_code=200,
     path='/login'
 )
 def user_login(
-    request: None,
+        request: None,
 ):
     pass
 
@@ -35,6 +42,6 @@ def user_login(
     path='/me'
 )
 def user_get_profile(
-    user_id: int = Depends(Authorization.bearer)
+        user_id: int = Depends(Authorization.bearer)
 ):
     pass
