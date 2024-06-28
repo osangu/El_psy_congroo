6510fad7930639dfda6b48d82b28f3c2ca6152d0
osangu <osangu.dev@gmail.com>
2024-06-28 15:07:31 +0900

    swagger detail

--- app/view/swagger.py
+++ app/view/swagger.py
@@ -1,18 +1,16 @@
 from enum import Enum
 from typing import List
 
-from pydantic import BaseModel
-
 
 class Tags(str, Enum):
     USER = 'User'
 
 
-class SwaggerDetail(BaseModel):
+class SwaggerDetail(dict):
     tags: List[Tags]
     summary: str
 
 
 class SwaggerDetails:
-    register = SwaggerDetail(summary='회원가입', tags=[Tags.USER])
     login = SwaggerDetail(summary='로그인', tags=[Tags.USER])
+    register = SwaggerDetail(summary='회원가입', tags=[Tags.USER])
