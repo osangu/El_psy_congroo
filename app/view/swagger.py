1b824855293f1f44089660d9dcf1859da27ede9d
osangu <osangu.dev@gmail.com>
2024-06-30 18:48:53 +0900

    SwaggerDetail init

--- app/view/swagger.py
+++ app/view/swagger.py
@@ -10,7 +10,14 @@ class SwaggerDetail(dict):
     tags: List[Tags]
     summary: str
 
+    def __init__(self, summary: str, tags: List[Tags]):
+        super().__init__()
+
+        self.tags = tags
+        self.summary = summary
+
 
 class SwaggerDetails:
     login = SwaggerDetail(summary='로그인', tags=[Tags.USER])
     register = SwaggerDetail(summary='회원가입', tags=[Tags.USER])
+    profile = SwaggerDetail(summary='내 정보 조회', tags=[Tags.USER])
