acb97d03342ffc217e46a586ac0dbde5d8751cfc
osangu <osangu.dev@gmail.com>
2025-02-10 19:41:49 +0900

    (#5) Struct: FastAPI - lifespan

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from contextlib import asynccontextmanager
+from typing import Callable
+from fastapi import FastAPI
+
+
+def create_app(lifespan: Callable):
+    app = FastAPI(lifespan=lifespan)
+
+    """
+    lifespan -  for infra (db, mq, ...)
+    create_app - for own app features
+    """
+
+    # include_routers()
+    # include_middlewares()
+
+    return app
+
+
+@asynccontextmanager
+def lifespan(_: FastAPI):
+    yield
