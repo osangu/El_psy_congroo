2ee7dfa49fcb1688d78e50a216041fef8f69a63d
osangu <osangu.dev@gmail.com>
2025-04-23 13:22:04 +0900

    test: temporary conftest

---
+++ test/conftest.py
@@ -0,0 +1,45 @@
+from contextlib import asynccontextmanager
+
+import pytest_asyncio
+from testcontainers.redis import RedisContainer
+from testcontainers.postgres import PostgresContainer
+
+from app import create_app
+
+
+@pytest_asyncio.fixture(scope='session')
+async def mock_postgres_url():
+    with PostgresContainer() as postgres:
+        yield postgres.get_connection_url()
+
+
+@pytest_asyncio.fixture(scope='session')
+async def mock_redis_conn_info():
+    password = 'qwer1234!!'
+
+    with RedisContainer(port=6379, password=password) as redis:
+        host = redis.get_container_host_ip()
+        port = redis.get_exposed_port(6379)
+
+        yield host, port, password
+
+
+@pytest_asyncio.fixture(scope='session')
+async def client(
+        mock_postgres_url,
+        mock_redis_conn_info
+):
+    redis_host, redis_port, redis_password = mock_redis_conn_info
+
+    @asynccontextmanager
+    async def lifespan():
+        init_rdb(mock_postgres_url)
+        init_cache_db(redis_host, redis_port, redis_password)
+
+        yield
+
+    app = create_app(lifespan=lifespan)
+
+    async with LifespanManager(app):
+        async with AsyncClient(app=app, base_url='https://test') as client:
+            yield client
